9fd287d7ebe5a0940f3c1f9bb051ffa39ea2b9d1
Cory Kane <ckane30691@gmail.com>
2018-05-04 20:46:38 -0700

    Set up initial app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+if (!process.env.JWT_SECRET) {
+  console.error(
+    'ERROR!: Please set JWT_SECRET before running the app. \n',
+    ' run: export JWT_SECRET=<some secret string> to set JWTSecret. '
+  );
+  process.exit();
+}
+
+const app = express();
+
+const logger = require('morgan');
+app.use(logger('dev'));
+
+// for parsing json and cookies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cookieParser());
+
+// CORS settings for dev
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Request-Headers", "*");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
