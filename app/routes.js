c9a7ead16a884bfeb54c8a85da5083fe5f05797f
Cory Kane <ckane30691@gmail.com>
2018-05-09 17:22:08 -0700

    Write routes for sessions

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,12 @@
+const userController = require('../controllers').userController;
+const sessionController = require('../controllers').sessionController;
+
 module.exports = function(app) {
   //root route
   app.get('/', function(req, res) {
     res.render('static_pages/index');
   });
+  app.post('/api/users', userController.create);
+  app.post('/api/session', sessionController.create);
+  app.delete('/api/session', sessionController.delete);
 };
