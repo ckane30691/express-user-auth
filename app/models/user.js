adec12b5796c87fea57ce1f00330d68ab0bb2762
Cory Kane <ckane30691@gmail.com>
2018-05-16 16:46:40 -0700

    Write resetSessionToken on user Model

--- app/models/user.js
+++ app/models/user.js
@@ -86,4 +86,10 @@ class Users {
         email.search(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/) === -1 ? false : true
       );
     }
+
+    resetSessionToken(user) {
+      let token = createJWToken();
+      user.sessionToken = token;
+      return this.update(user, token).then(() => token);
+    }
 }
