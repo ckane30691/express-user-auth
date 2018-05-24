069c7d0d06639c5c155a6b5ea3f79864b02d21da
Cory Kane <ckane30691@gmail.com>
2018-05-24 10:55:12 -0700

    write index.js file for db

---
+++ db/index.js
@@ -0,0 +1,44 @@
+const promise = require('bluebird');
+
+const models = require('../app/models'); // loading all repositories
+
+// pg-promise initialization options:
+const initOptions = {
+
+    // Use a custom promise library, instead of the default ES6 Promise:
+    promiseLib: promise,
+
+    // Extending the database protocol with our custom repositories;
+    // API: http://vitaly-t.github.io/pg-promise/global.html#event:extend
+    extend(obj, dc) {
+        // Database Context (dc) is mainly useful when extending multiple databases
+        // with different access API-s.
+
+        // Do not use 'require()' here, because this event occurs for every task
+        // and transaction being executed, which should be as fast as possible.
+        obj.users = new models.Users(obj, pgp);
+        obj.groups = new models.Groups(obj, pgp);
+    }
+};
+
+// Database connection parameters:
+const config = {
+    host: 'localhost',
+    port: 5432,
+    database: 'UserGroupApi',
+    user: 'postgres'
+};
+
+// Load and initialize pg-promise:
+const pgp = require('pg-promise')(initOptions);
+
+// Create the database instance:
+const db = pgp(config);
+
+// Load and initialize optional diagnostics:
+// const diagnostics = require('./diagnostics');
+// diagnostics.init(initOptions);
+
+// If you ever need access to the library's root (pgp object), you can do it via db.$config.pgp
+// See: http://vitaly-t.github.io/pg-promise/Database.html#.$config
+module.exports = db;
