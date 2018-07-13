e61b1ebd69ee694138cef8fbdbbf46a7aa90a254
Cory Kane <ckane30691@gmail.com>
2018-07-12 21:57:34 -0700

    Clean up reducers

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
 
 import session from './session_errors_reducer';
-import group from './group_errors_reducer';
 
 export default combineReducers({
   session,
-	group,
 });
